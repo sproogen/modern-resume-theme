221b568729c88acf53f060ae87539f8fdd4d08e6
James Grant <sprog31@gmail.com>
2018-06-12 00:08:37 +0100

    Added bump version script

---
+++ scripts/bump-version.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+set -ev
+
+if [ "$TRAVIS_BRANCH" == "master" ] && [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
+    gem install gem-release --pre;
+
+    git config --global user.email ${EMAIL};
+    git config --global user.name ${USER};
+
+    git checkout master;
+
+    gem bump --skip-ci;
+    gem tag --no-push;
+
+    git push "https://${GITHUB_TOKEN}@${GITHUB_REPO}" master --tags;
+fi
--- scripts/merge-downstream.sh
+++ scripts/merge-downstream.sh
@@ -16,7 +16,7 @@ if [ "$TRAVIS_BRANCH" == "master" ] && [ "$TRAVIS_PULL_REQUEST" == "false" ]; th
 
         curl -H "Authorization: token ${GITHUB_TOKEN}" -X POST -d '{"title":"Upstream merge conflict", "base":"develop", "head":"master"}' "https://api.github.com/repos/${TRAVIS_REPO_SLUG}/pulls";
     else
-        git push "https://${GITHUB_TOKEN}@${GITHUB_REPO}" develop --tags;
+        git push "https://${GITHUB_TOKEN}@${GITHUB_REPO}" develop;
     fi
 
     git checkout master;
