b7b6da4179f15e7a2f6926064915ca980db83273
James Grant <sprog31@gmail.com>
2018-06-11 22:45:20 +0100

    Moved to merge-downstream script

---
+++ scripts/merge-downstream.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+if [ "$TRAVIS_BRANCH" == "master" ] && [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
+    git remote set-branches --add origin develop;
+
+    git checkout develop;
+    git pull origin develop;
+
+    git merge master;
+
+    if [[ ("$?" -ne "0") ]]; then
+        git merge --abort;
+        git reset --hard;
+
+        curl -H "Authorization: token ${GITHUB_TOKEN}" -X POST -d '{"title":"Upstream merge conflict", "base":"develop", "head":"master"}' "https://api.github.com/repos/${TRAVIS_REPO_SLUG}/pulls";
+    else
+        git push "https://${GITHUB_TOKEN}@${GITHUB_REPO}" develop --tags;
+    fi
+
+    git checkout master;
+fi
