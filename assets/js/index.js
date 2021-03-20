9b12e6913d4c5eaf894968780bef9015a0e3ec1a
demon-rem <22884507+demon-rem@users.noreply.github.com>
2021-03-20 22:43:05 +0530

    Update: Allow visitors to toggle dark theme
    Add a new sticky button to the bottom-right corner allowing
    visitors to toggle dark mode on their end. For first-time
    visitors, the value defaults to the value set in
    `_configs.yml/darkmode`.
    
    Stores a cookie for the theme that will be used in subsequent
    visits. In case multiple sessions to the website are opened,
    changing the mode in any once instace will reflect across all
    tabs.
    
    This feature can be turned off by modifying the value of
    `_config.yml/darktoggle`.
    
    Move the existing contents of `/assets/index.js` as an inline
    script.

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,3 +1,102 @@
-if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
-  document.body.classList.add("dark");
+// Deals with enabling/disabling dark theme when required.
+
+var theme = "theme";
+var dark_mode = "dark";
+var light_mode = "light";
+
+/**
+ * A simple wrapper to modify the value of cookies used to keep a track of 
+ * website theme.
+ * 
+ * @param {boolean} enableDarkMode Modifies the value of cookies to 
+ * enable/disable dark mode.
+ * @returns none
+ */
+function modifyCookies(enableDarkMode) {
+    window.localStorage.setItem(theme, enableDarkMode ? dark_mode : light_mode);
 }
+
+/**
+ * Wrapper function to read cookes and check if dark mode is enabled or not.
+ * 
+ * @returns {String} String value containing either `dark_mode`, `light_mode` or null
+ */
+function getCurTheme() {
+    return window.localStorage.getItem(theme);
+}
+
+/**
+ * Function to modify button being used in the dark theme icon.
+ * 
+ * @param {boolean} darkThemeEnabled Boolean indicating if dark theme
+ * has been switched on or off.
+ */
+function toggleButtonIcon(darkThemeEnabled) {
+    let button = document.getElementById("dark-theme-button");
+
+    if (darkThemeEnabled) {
+        button.classList.remove("fas");
+        button.classList.add("far");
+    } else {
+        button.classList.add("fas");
+        button.classList.remove("far");
+    }
+}
+
+/**
+ * Initial setup method - will read the value of stored cookies to check 
+ * if dark mode is enabled or not.
+ * 
+ * Does not make any changes to the values of cookies. Function designed 
+ * to be run when the web-page loads, will simply read the value of existing
+ * cookies (if any), and enable dark mode if required.
+ */
+function setupTheme() {
+    console.log("value: " + getCurTheme());
+
+    // If neither one of the if-else ladder condition is true, do not modify
+    // the theme - ensures that if the user is visiting for the first time,
+    // the theme defaults to the value of `_config.yml/darkmode`
+    if (getCurTheme() === dark_mode)
+        // Dark mode is required
+        document.body.classList.add(dark_mode); // enable dark mode
+    else if (getCurTheme() === light_mode)
+        // Note: Intentionally not removing the element from class list. Ensures
+        // functionality of `darkmode` variable in _configs.yml doesn't break.
+        document.body.classList.remove(dark_mode); // disable dark mode
+
+    // Modify/apply icon for the theme button
+    if (document.body.classList.contains(dark_mode))
+        toggleButtonIcon(true);
+    else
+        toggleButtonIcon(false);
+
+}
+
+/**
+ * Toggles the website theme between dark-mode and normal.
+ */
+function modifyTheme() {
+    if (getCurTheme() === dark_mode) {
+        // Current mode is dark mode, switch to light
+        window.localStorage.setItem(theme, light_mode);
+        document.body.classList.remove(dark_mode);
+        toggleButtonIcon(false);
+    } else {
+        // Assume dark mode is being turned off, enable it
+        window.localStorage.setItem(theme, dark_mode);
+        document.body.classList.add(dark_mode);
+        toggleButtonIcon(true);
+    }
+}
+
+/**
+ * Adding an event listener to detect when the webpage is brought back
+ * in focus. Ensures synced website theme across multiple simultaneous 
+ * sessions, i.e. if the theme is changed in one tab - other tabs will
+ * sync with the changes as soon as they are brought back in focus.
+ */
+document.addEventListener("visibilitychange", function(val) {
+    if (!document.hidden)
+        setupTheme();
+});
