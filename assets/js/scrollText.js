eb37a77158c43da785186696eb6c2b4d7e8a88d2
Cassia <cassia_jiang@163.com>
2023-06-23 12:21:24 +0800

    feat: update content

---
+++ assets/js/scrollText.js
@@ -0,0 +1,14 @@
+// const colorSpace = document.querySelector('.color-space');
+// const scrollingText = document.querySelector('.scrolling-text');
+
+// function handleOverflow() {
+//   if (scrollingText.offsetWidth < scrollingText.scrollWidth) {
+//     console.log('hhhh')
+//     colorSpace.classList.add('overflowed');
+//   } else {
+//     console.log('ssss')
+//     colorSpace.classList.remove('overflowed');
+//   }
+// }
+
+// handleOverflow();
